5031d1d2dba2f722fabc7cb5fa6487001f064578
Dhaval Bodar <dhaval4225+github@gmail.com>
2025-09-01 09:59:01 +0530

    user daily limit add

--- controller/userAuth/login.js
+++ controller/userAuth/login.js
@@ -1,6 +1,9 @@
 const User = require("../../model/user.model");
+const History = require("../../model/tokenhistory.model");
 const shortid = require('shortid');
 const moment = require("moment");
+const { Op, fn, col, where } = require("sequelize");
+const { checkToken } = require("../../helper/common");
 
 
 exports.login = async (req, res) => {
@@ -33,7 +36,47 @@ exports.login = async (req, res) => {
                     },
                     { where: { deviceId: deviceId } },
                 )
+            }
 
+            if (findUser.planType === "Free-Plan" && findUser.reminToken == 0) {
+                const dailyLimit = process.env.DAILY_LIMIT
+
+                const lastEntry = await History.findAll({
+                    where: {
+                        deviceId: deviceId,
+                        apiProvider: "dailyLimit",
+                        [Op.and]: where(fn("DATE", col("useDateTime")), fn("CURDATE")),
+                    },
+                    order: [["id", "DESC"]],
+                });
+                if (!lastEntry.length > 0) {
+
+                    await User.update(
+                        {
+                            totalToken: Number(findUser.totalToken) + Number(dailyLimit),
+                            reminToken: dailyLimit,
+                        },
+                        { where: { deviceId: deviceId } },
+                    )
+
+                    // Create History 
+                    await History.create({
+                        apiProvider: "dailyLimit",
+                        deviceId: deviceId,
+                        totalToken: Number(findUser.totalToken) + Number(dailyLimit),
+                        usedToken: findUser.usedToken,
+                        reminToken: dailyLimit,
+                        metadata: "dailyLimit"
+                    })
+                    const dailyFreshUserDetails = await checkToken(deviceId);
+                    return res.status(200).json({
+                        data: dailyFreshUserDetails
+                    });
+                } else {
+                    return res.status(200).json({
+                        data: findUser.toJSON()
+                    });
+                }
             }
             res.status(200).json({
                 data: findUser.toJSON()
