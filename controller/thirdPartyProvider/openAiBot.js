e18ad6228144d55478a2540c6cfe0a93b86a1262
Om <yesiamom28@gmail.com>
2025-10-21 14:12:37 +0530

    bot assi changes

--- controller/thirdPartyProvider/openAiBot.js
+++ controller/thirdPartyProvider/openAiBot.js
@@ -2,6 +2,12 @@ const axios = require("axios");
 const { checkToken, reduceToken } = require("../../helper/common");
 const { pick } = require("lodash");
 const { getToken } = require("../../config/manageToken");
+const summary_Ass_ID = "asst_8XphHk5hHVolGxVPfMkQqf5i"
+const spell_Ass_ID = "asst_Yshc3HmPy5zJuCbTAcjze0r1"
+
+// type
+//     summarizerBot
+//     spellCheckerBot
 
 exports.aiBot = async (req, res) => {
     try {
@@ -17,9 +23,10 @@ exports.aiBot = async (req, res) => {
         // await reduceToken(deviceId, uniqueId, "Bot", type, true)
 
         if (threadId && role && content) {
-            if (type === "summarizerBot") {
-                try {
-                    let runSummari = await axios({
+            const assistantId = type === "summarizerBot" ? summary_Ass_ID : spell_Ass_ID
+            try {
+                const newSummriRes = {}
+                let runSummari = await axios({
                     url: `https://api.openai.com/v1/threads/${threadId}/messages`,
                     method: 'post',
                     headers: {
@@ -31,55 +38,114 @@ exports.aiBot = async (req, res) => {
                         role: role,
                         content: content
                     }
-                    });
+                });
+                newSummriRes["id"] = runSummari.data.id
 
-                    const pickRunSummari = pick(runSummari.data, ['id', 'content'])
-                    pickRunSummari.content = pickRunSummari.content.map(c => ({ text: pick(c.text, ['value']), type: c.type }));
-                    runSummari = pickRunSummari
-
-                    runSummari["userDetails"] = apiSendUserDetails
-
-                    res.status(200).json({
-                        data: runSummari
-                    })
-                } catch (error) {
-                    res.status(400).json({
-                        message: error.response.data.error.message
-                    })
-                }
-
-            }
-
-            if (type === "spellCheckerBot") {
-                try {
-                    let runSpellChecker = await axios({
-                    url: `https://api.openai.com/v1/threads/${threadId}/messages`,
-                    method: 'post',
-                    headers: {
-                        Authorization: `Bearer ${apiSendUserDetails.isSubscribe == 1 ? spellCheckerBotToken.subscribe_token : spellCheckerBotToken.token}`,
-                        'OpenAI-Beta': 'assistants=v2',
-                        'Content-Type': 'application/json'
+                const runSummRes = await axios.post(
+                    `https://api.openai.com/v1/threads/${threadId}/runs`,
+                    {
+                        assistant_id: assistantId,
                     },
-                    data: {
-                        role: role,
-                        content: content
+                    {
+                        headers: {
+                            Authorization: `Bearer ${apiSendUserDetails.isSubscribe == 1 ? summarizerBotToken.subscribe_token : summarizerBotToken.token}`,
+                            'OpenAI-Beta': 'assistants=v2',
+                            'Content-Type': 'application/json',
+                        },
                     }
-                    });
+                );
+                const runUploadId = runSummRes.data.id;
+                
+                let runStatus = 'queued';
+                while (runStatus !== 'completed') {
+                    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-                    const pickSpellChecker = pick(runSpellChecker.data, ['id', 'content'])
-                    pickSpellChecker.content = pickSpellChecker.content.map(c => ({ text: pick(c.text, ['value']), type: c.type }));
-                    runSpellChecker = pickSpellChecker
-                    runSpellChecker["userDetails"] = apiSendUserDetails
+                    try {
+                        const pollRes = await axios.get(
+                            `https://api.openai.com/v1/threads/${threadId}/runs/${runUploadId}`,
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${apiSendUserDetails.isSubscribe == 1 ? summarizerBotToken.subscribe_token : summarizerBotToken.token}`,
+                                    'OpenAI-Beta': 'assistants=v2',
+                                    'Content-Type': 'application/json'
+                                },
+                            }
+                        );
 
-                    res.status(200).json({
-                        data: runSpellChecker
-                    })
-                } catch (error) {
-                    res.status(400).json({
-                        message: error.response.data.error.message
-                    })
+                        runStatus = pollRes.data.status;
+                        console.log("--runStatus summari----", runStatus)
+                        if (runStatus === 'failed' || runStatus === 'cancelled') {
+                            res.status(400).json({
+                                message: `Run ${runStatus}`,
+                                status: 400
+                            })
+                            return;
+                        }
+                    } catch (error) {
+                        console.log("---error--", error);
+                        res.status(400).json({
+                            message: error.response.data.error.message,
+                            status: 400
+                        });
+                        return;
+                    }
                 }
+
+                let answerRes = await axios.get(
+                    `https://api.openai.com/v1/threads/${threadId}/messages`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${apiSendUserDetails.isSubscribe == 1 ? summarizerBotToken.subscribe_token : summarizerBotToken.token}`,
+                            'OpenAI-Beta': 'assistants=v2',
+                            'Content-Type': 'application/json'
+                        },
+                    }
+                );
+                answerRes = answerRes.data
+                newSummriRes["content"] = [{
+                    role: answerRes.data[0].role,
+                    text: answerRes.data[0]["content"][0]["text"]["value"] || ""
+                }];
+                newSummriRes["userDetails"] = apiSendUserDetails
+
+                res.status(200).json({
+                    data: newSummriRes
+                })
+            } catch (error) {
+                res.status(400).json({
+                    message: error.response.data.error.message
+                })
             }
+            // if (type === "spellCheckerBot") {
+            //     try {
+            //         let runSpellChecker = await axios({
+            //         url: `https://api.openai.com/v1/threads/${threadId}/messages`,
+            //         method: 'post',
+            //         headers: {
+            //             Authorization: `Bearer ${apiSendUserDetails.isSubscribe == 1 ? spellCheckerBotToken.subscribe_token : spellCheckerBotToken.token}`,
+            //             'OpenAI-Beta': 'assistants=v2',
+            //             'Content-Type': 'application/json'
+            //         },
+            //         data: {
+            //             role: role,
+            //             content: content
+            //         }
+            //         });
+
+            //         const pickSpellChecker = pick(runSpellChecker.data, ['id', 'content'])
+            //         pickSpellChecker.content = pickSpellChecker.content.map(c => ({ text: pick(c.text, ['value']), type: c.type }));
+            //         runSpellChecker = pickSpellChecker
+            //         runSpellChecker["userDetails"] = apiSendUserDetails
+
+            //         res.status(200).json({
+            //             data: runSpellChecker
+            //         })
+            //     } catch (error) {
+            //         res.status(400).json({
+            //             message: error.response.data.error.message
+            //         })
+            //     }
+            // }
 
         } else {
             res.status(400).json({
@@ -89,7 +155,6 @@ exports.aiBot = async (req, res) => {
 
 
     } catch (error) {
-        console.log(error);
         res.status(500).json({
             message: "Something went wrong",
             status: 500
