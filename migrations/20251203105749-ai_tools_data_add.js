fe4a2ae5dadb908743d850bf429b2ee490e12197
Dhaval <dhaval4225+github@gmail.com>
2025-12-03 16:35:41 +0530

    add tool in add assitance id

---
+++ migrations/20251203105749-ai_tools_data_add.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      UPDATE "aiTool"
+      SET "assistantId" = 'asst_8XphHk5hHVolGxVPfMkQqf5i'
+      WHERE "nameAssistant" = 'Text Summarizer';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE "aiTool"
+      SET "assistantId" = 'asst_Yshc3HmPy5zJuCbTAcjze0r1'
+      WHERE "nameAssistant" = 'Spell Checker';
+    `);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      UPDATE "aiTool"
+      SET "assistantId" = NULL
+      WHERE "nameAssistant" = 'Text Summarizer';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE "aiTool"
+      SET "assistantId" = NULL
+      WHERE "nameAssistant" = 'Spell Checker';
+    `);
+  }
+};
