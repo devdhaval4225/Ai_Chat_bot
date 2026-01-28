5075ba4870c199d7afa6a381e85fe8e0ce01ced4
Dhaval <dhaval4225+github@gmail.com>
2026-01-28 21:56:37 +0530

    add image creation related changes

--- controller/imageCreation/imageCreate.js
+++ controller/imageCreation/imageCreate.js
@@ -13,8 +13,10 @@ exports.imageCreateProvider = async (req, res) => {
     try {
         const uniqueId = req.headers.uniqueid
         const body = req.body
-        const { deviceId, apiProvider, modelId, message, size, quality } = body
+        const { deviceId, apiProvider, modelId, size, quality } = body
         let updateUserData
+
+        let inputObj = []
 
         const notusedToken = await checkToken(body.deviceId)
         let apiSendUserDetails = pick(notusedToken, ['id', 'totalToken', 'usedToken', 'reminToken', 'planType', 'isSubscribe', 'expireDate']);
@@ -38,45 +40,86 @@ exports.imageCreateProvider = async (req, res) => {
                 content: {},
                 userDetails: apiSendUserDetails
             }
+            let imageUrl
 
+            const messObj = await this.apiInputObj(apiProvider, body)
             if (apiProvider === "openAi") {
                 try {
-                    const imageBody = {
-                        "model": model,
-                        "prompt": message,
-                        "n": 1,
-                    }
+                    if (messObj.isBuffer && messObj.isBuffer == true) {
 
-                    if (body && body.size) {
-                        imageBody["size"] = size
-                    }
-                    if (body && body.quality) {
-                        imageBody["quality"] = quality
-                    }
+                        const messArr = messObj.obj.filter((v) => v.type == "input_text").map((v) => v.text)
+                        const FormData = require("form-data");
+                        const form = new FormData();
+                        form.append("model", model);
+                        form.append("prompt", messArr.join(","));
+                        if (body && body.size) { form.append("size", size) }
+                        if (body && body.quality) { form.append("quality", quality) }
 
-                    const createImg = await axios({
-                        url: 'https://api.openai.com/v1/images/generations',
-                        method: 'post',
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                            'Content-Type': 'application/json'
-                        },
-                        data: imageBody
-                    });
-                    await reduceToken(deviceId, uniqueId, apiProvider, "createImageOpenAi", true, rToken);
-                    let imageUrl
-                    if (createImg.data.data[0] && createImg.data.data[0]["b64_json"]) {
-                        const imageData = createImg.data.data[0]["b64_json"]
-                        const imageMimeType = createImg.data["output_format"]
-                        imageUrl = await this.imageCreateViaBase(imageData, imageMimeType, "openai")
+                        const imageArr = messObj.obj.filter((v) => v.type == "input_image")
+                        if (imageArr.length > 0) {
+                            form.append(
+                                "image",
+                                Buffer.from(imageArr[0]["image_url"].replace(/^data:image\/\w+;base64,/, ""), "base64"),
+                                {
+                                    filename: `image.png`,
+                                    contentType: "image/png",
+                                }
+                            );
+                        }
+                        const response = await axios.post(
+                            "https://api.openai.com/v1/images/edits",
+                            form,
+                            {
+                                headers: {
+                                    ...form.getHeaders(),
+                                    Authorization: `Bearer ${token}`,
+                                },
+                                maxBodyLength: Infinity,
+                            }
+                        );
+
+                        if (response.data.data[0] && response.data.data[0]["b64_json"]) {
+                            const imageOldData = response.data.data[0]["b64_json"]
+                            const imageMimeType = response.data["output_format"]
+                            imageUrl = await this.imageCreateViaBase(imageOldData, imageMimeType, "openai")
+                        } else {
+                            imageUrl = response.data.data[0]["url"]
+                        }
+                        resOpenObj["content"] = { url: imageUrl }
+                        res.status(200).json({ data: resOpenObj })
+
                     } else {
-                        imageUrl = createImg.data.data[0]["url"]
+                        const messSecondArr = messObj.obj.filter((v) => v.type == "input_text").map((v) => v.text).join(",")
+                        const imageBody = {
+                            "model": model,
+                            "prompt": messSecondArr,
+                            "n": 1,
+                        }
+
+                        if (body && body.size) { imageBody["size"] = size }
+                        if (body && body.quality) { imageBody["quality"] = quality }
+
+                        const createImg = await axios({
+                            url: 'https://api.openai.com/v1/images/generations',
+                            method: 'post',
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                                'Content-Type': 'application/json'
+                            },
+                            data: imageBody
+                        });
+                        await reduceToken(deviceId, uniqueId, apiProvider, "createImageOpenAi", true, rToken);
+                        if (createImg.data.data[0] && createImg.data.data[0]["b64_json"]) {
+                            const imageData = createImg.data.data[0]["b64_json"]
+                            const imageMimeType = createImg.data["output_format"]
+                            imageUrl = await this.imageCreateViaBase(imageData, imageMimeType, "openai")
+                        } else {
+                            imageUrl = createImg.data.data[0]["url"]
+                        }
+                        resOpenObj["content"] = { url: imageUrl }
+                        res.status(200).json({ data: resOpenObj })
                     }
 
-                    resOpenObj["content"] = {
-                        url: imageUrl
-                    }
-                    res.status(200).json({ data: resOpenObj }) // createImg.data
                 } catch (error) {
                     res.status(400).json({ message: error.response.data.error.message })
                 }
@@ -85,22 +128,13 @@ exports.imageCreateProvider = async (req, res) => {
             if (apiProvider === "Gemini") {
                 try {
                     const inputObj = {
-                        contents: [{
-                            parts: [{
-                                text: message
-                            }]
-                        }],
+                        contents: messObj.obj.contents,
                         "generationConfig": { "responseModalities": ["IMAGE"] }
                     };
 
-                    if (body && body.quality) {
-                        inputObj["generationConfig"]["imageConfig"] = { imageSize: body.quality }
-                    }
+                    if (body && body.quality) { inputObj["generationConfig"]["imageConfig"] = { imageSize: body.quality } }
+                    if (body && body.size) { inputObj["generationConfig"]["imageConfig"] = { aspectRatio: body.size } }
 
-                    if (body && body.size) {
-                        inputObj["generationConfig"]["imageConfig"] = { aspectRatio: body.size }
-                    }
-                    
                     let chatCompletions = await axios({
                         url: `https://generativelanguage.googleapis.com/v1beta/models/${model}`,
                         method: 'post',
@@ -110,14 +144,12 @@ exports.imageCreateProvider = async (req, res) => {
                         },
                         data: inputObj
                     });
-                    const { mimeType, data } = chatCompletions.data.candidates[0]["content"]["parts"][0]["inlineData"]
                     await reduceToken(deviceId, uniqueId, apiProvider, "createImageGemini", true, rToken);
-
+                    
+                    const { mimeType, data } = chatCompletions.data.candidates[0]["content"]["parts"][0]["inlineData"]
                     const imageUrl = await this.imageCreateViaBase(data, mimeType, "gemini")
 
-                    const content = {
-                        url: imageUrl
-                    }
+                    const content = { url: imageUrl }
 
                     updateUserData = await checkToken(deviceId)
                     const resChatCompletions = {
@@ -177,5 +209,74 @@ exports.imageCreateViaBase = async (data, mimeType, name) => {
         return url = `${baseUrl}public/${fileName}`;
     } catch (error) {
         console.log("---inout creation error ---", error)
+    }
+};
+
+exports.apiInputObj = async (type, body) => {
+    try {
+        let finalMessageObj = {}
+
+        let isBuffer = false
+        if (type == "openAi") {
+            const role = body.message[0]["role"]
+            finalMessageObj["role"] = role
+
+            const openMessArr = []
+            for (let i = 0; i < body.message.length; i++) {
+                let openMessObj = {}
+
+                const type = body.message[i]["type"];
+                const text = body.message[i]["text"];
+                const imageData = body.message[i]["imageData"];
+
+                if (type == "image") {
+                    isBuffer = true
+                    openMessObj["type"] = "input_image"
+                    openMessObj["image_url"] = imageData
+                } else {
+                    openMessObj["text"] = text
+                    openMessObj["type"] = "input_text"
+                }
+
+                openMessArr.push(openMessObj)
+                openMessObj = {}
+            }
+            finalMessageObj = openMessArr
+            return { obj: finalMessageObj, isBuffer: isBuffer };
+
+        }
+
+        if (type == "Gemini") {
+            const role = body.message[0]["role"]
+            // finalMessageObj["role"] = role
+
+            const geminiMessArr = []
+            for (let i = 0; i < body.message.length; i++) {
+                let geminiMessObj = {}
+
+                const type = body.message[i]["type"];
+                const text = body.message[i]["text"];
+                const imageData = body.message[i]["imageData"];
+                const mimeType = body.message[i]["mimeType"];
+
+                if (type == "image") {
+                    isBuffer = true
+                    geminiMessObj["inlineData"] = {
+                        mime_type: mimeType,
+                        data: imageData
+                    }
+                } else {
+                    geminiMessObj["text"] = text
+                }
+                geminiMessArr.push(geminiMessObj)
+                geminiMessObj = {}
+            }
+            finalMessageObj["contents"] = [{ parts: geminiMessArr }]
+            return { obj: finalMessageObj, isBuffer };
+
+        }
+
+    } catch (error) {
+        console.log("---input creation error ---", error)
     }
 };
